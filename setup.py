2bb1b1908504f0c4dea823d36460e9c59e8b3915
Chris Berry <chris@chrisberry.com.au>
2019-11-12 14:57:55 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='django-handy-converters',
-      version='2019.1',
+      version='2019.2',
       description='Some handy URL converters for django',
       packages=[
         'handy_converters'
